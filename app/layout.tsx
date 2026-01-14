d971cd9c934467f7eb0d97544d62ddf3c0639c20
Lazar Vidojevic <lazzar.vidojevic@gmail.com>
2026-01-14 23:09:15 +0100

    Fix imports for layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import ClientThemeProvider from "@/components/ClientThemeProvider";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ClientThemeProvider from "../components/ClientThemeProvider";
 
 // Google Fonts
 const geistSans = Geist({
